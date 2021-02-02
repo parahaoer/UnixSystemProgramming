48a3a604dbe128a56432216aab24458d569ecc13
parahaoer <884101054@qq.com>
2021-02-02 13:46:51 +0800

    使用C标准库IO接口拷贝文件

--- IO/copyFileWithbufferedIO.c
+++ IO/copyFileWithbufferedIO.c
@@ -1,36 +1,39 @@
-#include <sys/types.h>
-#include <sys/stat.h>  // 提供mode_t类型
-#include<fcntl.h>
 #include<stdio.h>
+#include<stdlib.h>
 
 #define MAX_BUFFER_LEN 20
 
 int main(int argc, char const *argv[])
 {
     char buffer[MAX_BUFFER_LEN];
-    int fd = open("test.txt", O_RDONLY);
-    if (fd < 0) {
+    FILE *input = fopen("test.txt", "r");
+    if (input == NULL) {
         perror("open srcFile error");
-    }
-    // O_WRONLY表示只写，O_CREAT表示文件不存在则创建，O_TRUNC表示清空文件内容，将其长度置为0
-    int dstFd = open("dst.txt", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR);
-    if (dstFd < 0) {
-        perror("open dstFile error");
-    }
-    int len;
-    while ((len = read(fd, buffer, MAX_BUFFER_LEN)) >= 0) {
-        if (len == 0) break;
-        int ret = write(dstFd, buffer, len);
-        if (ret < 0) {
-            perror("write error");
-        }
-    }
-    if (len < 0) {
-        perror("read error");
+        exit(1);
     }
 
-    close(dstFd);
-    close(fd);
+    FILE *output = fopen("dst.txt", "w");
+    if (output == NULL) {
+        perror("open dstFile error");
+        exit(1);
+    }
+    size_t readLen;
+
+    while ((readLen = fread(buffer, 1, MAX_BUFFER_LEN, input)) >= 0) {
+
+        // 将缓冲区buffer中的数据写入文件output中, buffer是待写入缓冲区的地址（指针), 1 是待写入数据块的大小， readLen是待写入数据块的个数。output是待写入的文件。
+        // fwrite()函数返回值是正常写入数据块的个数。如果出错，返回值会小于readLen
+        int ret = fwrite(buffer, 1, readLen, output);
+        if (ret < readLen) {
+            perror("write error");
+            exit(1);
+        }
+        // 当读取出错或者读到文件结尾，fread函数的返回值readLen小于MAX_BUFFER_LEN（即缓冲区数组的大小）
+        if (readLen < MAX_BUFFER_LEN) break;
+    }
+
+    fclose(output);
+    fclose(input);
 
     return 0;
 }
